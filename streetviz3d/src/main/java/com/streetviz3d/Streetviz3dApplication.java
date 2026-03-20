3ad9657ec4f567e116142232a8301af9dcd6f00f
LaoeGaoci <1523796237@qq.com>
2026-03-20 10:47:09 +0800

    Init backend

--- streetviz3d/src/main/java/com/streetviz3d/Streetviz3dApplication.java
+++ streetviz3d/src/main/java/com/streetviz3d/Streetviz3dApplication.java
@@ -1,13 +1,13 @@
-package com.streetvize3d.backend;
+package com.streetviz3d;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class Streetviz3dApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
+        SpringApplication.run(Streetviz3dApplication.class, args);
     }
 
 }
